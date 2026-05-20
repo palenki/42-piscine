e65f0c29a0b69795abb8a7041fe219c19d6db510
Paulo Santos <paulo_henrique.st@hotmail.com>
2026-05-20 15:26:26 -0300

    Implement bubble sort to solve ft_sort_params

--- piscine-reloaded/ex19/ft_sort_params.c
+++ piscine-reloaded/ex19/ft_sort_params.c
@@ -6,21 +6,17 @@
 /*   By: pauhenr2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 10:08:06 by pauhenr2          #+#    #+#             */
-/*   Updated: 2026/05/19 16:45:53 by pauhenr2         ###   ########.fr       */
+/*   Updated: 2026/05/20 14:59:34 by pauhenr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
-/int	ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 
-	i = 1;
+	i = 0;
 	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
@@ -33,10 +29,23 @@ void	ft_putchar(char c)
 void	ft_swap(char **a, char **b)
 {
 	char	*temp;
-	
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
 }
 
 int	main(int argc, char **argv)
@@ -47,13 +56,20 @@ int	main(int argc, char **argv)
 	i = 1;
 	while (i < argc)
 	{
-		j = 0;
-		while (argv[i][j])
+		j = 1;
+		while (j < argc -1)
 		{
-			ft_putchar(argv[i][j]);
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+				ft_swap(&argv[j], &argv[j + 1]);
 			j++;
 		}
 		i++;
-		ft_putchar('\n');
 	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+	return (0);
 }
